199a878e7a00c5c346dc0afeeb8a41cd6a986e0c
Jarrod Angove <jarrod_angove@hotmail.com>
2024-04-15 20:42:21 -0600

    got background working and fixed hardware config bug

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+# enter the following to start setup:
+# sudo chmod +x ./setup.sh
+# sudo ./setup.sh
+
 # remove the empty nix config from etc so symlink can be created from dotfiles directory
 sudo rm -f /etc/nixos/configuration.nix
 
@@ -7,8 +11,11 @@ sudo rm -f /etc/nixos/configuration.nix
 # another machine to be configured to another system
 sudo rm -f ~/dotfiles/hardware-configuration.nix
 
+sudo cp /etc/nixos/hardware-configuration.nix ~/dotfiles/hardware-configuration.nix
+sudo ln -fs ~/dotfiles/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
 # hardware config is the only config I am leaving in its original place
-sudo ln -fs /etc/nixos/hardware-configuration.nix ~/dotfiles/hardware-configuration.nix
+sudo ln -fs 
 
 sudo ln -fs ~/dotfiles/configuration.nix /etc/nixos/configuration.nix
 sudo ln -fs ~/dotfiles/flake.nix /etc/nixos/flake.nix
